35ba4c18079afc4f85159aaecc207abef26b8dda
joaom <joao.p.f.martins2005@gmail.com>
2025-01-07 23:37:04 +0000

    Created Class Book (still very WIP)

---
+++ LivrariaOnline/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace LivrariaOnline.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public string Publisher { get; set; }
+        public string ISBN { get; set; }
+        public string Language { get; set; }
+        public int Pages { get; set; }
+        public string Description { get; set; }
+        public string CoverImage { get; set; }
+        public decimal Price { get; set; }
+    }
+}
