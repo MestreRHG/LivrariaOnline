2d53fb3150fe06edf70cf756e9bd94ad181301ff
Fabio Ramos <fetr1205@gmail.com>
2025-01-09 17:18:45 +0000

    fix to be able to customize login

---
+++ LivrariaOnline/Views/Home/BooksInformation.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LivrariaOnline.Views.Home
+{
+    public class BooksInformationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
