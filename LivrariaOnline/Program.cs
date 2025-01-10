7e8ddcdec98ba0de21bce383f8f32c7e0acb19a7
Fabio Ramos <fetr1205@gmail.com>
2025-01-10 09:55:15 +0000

    add field username

--- LivrariaOnline/Program.cs
+++ LivrariaOnline/Program.cs
@@ -8,8 +8,8 @@ namespace LivrariaOnline
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
+            var builder = WebApplication.CreateBuilder(args); 
+            
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
