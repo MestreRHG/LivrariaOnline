05e0d4f117fd831e9e1c4884baf7e743e6d6c116
Fabio Ramos <fetr1205@gmail.com>
2025-01-13 14:18:22 +0000

    add new controler

---
+++ LivrariaOnline/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using LivrariaOnline.Data;
+using Microsoft.AspNetCore.Mvc;
+
+public class BooksController : Controller
+{
+	private readonly ApplicationDbContext _context;
+
+	public IActionResult Details(int id)
+	{
+		var book = _books.FirstOrDefault(l => l.Id == id);
+		if (livro == null)
+		{
+			return NotFound();
+		}
+
+		var viewModel = new BookDetailsViewModel { Book = livro };
+		return View(viewModel);
+	}
+
+}
+
