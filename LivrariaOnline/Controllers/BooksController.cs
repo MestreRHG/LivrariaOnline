a6248d841c8f7694b7529cc2e863ced0a51114b2
João Martins <joao.p.f.martins2005@gmail.com>
2025-01-13 14:45:38 +0000

    Changed the details page to it's own folder and changed the controller

--- LivrariaOnline/Controllers/BooksController.cs
+++ LivrariaOnline/Controllers/BooksController.cs
@@ -1,21 +1,50 @@
-﻿using LivrariaOnline.Data;
+﻿using LivrariaOnline.Controllers;
+using LivrariaOnline.Data;
+using LivrariaOnline.Models;
 using Microsoft.AspNetCore.Mvc;
 
 public class BooksController : Controller
 {
-	private readonly ApplicationDbContext _context;
+    private readonly ILogger<BooksController> _logger;
+    private readonly ApplicationDbContext _context;
 
-	public IActionResult Details(int id)
+    public BooksController(ILogger<BooksController> logger, ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public IActionResult Details(int id)
 	{
-		var book = _books.FirstOrDefault(l => l.Id == id);
-		if (livro == null)
+		var book = _context.Books.First(l => l.Id == id);
+		if (book == null)
 		{
-			return NotFound();
+            ViewData["Title"] = "Create a Book";
+            return View();
 		}
 
-		var viewModel = new BookDetailsViewModel { Book = livro };
-		return View(viewModel);
+        ViewData["Title"] = "Edit a Book";
+        return View(book);
 	}
+
+    // Function to save or edit the book
+    [HttpPost]
+    public IActionResult Save(Book book)
+    {
+        var bookInDb = _context.Books.First(l => l.Id == book.Id);
+
+        if (bookInDb == null)
+        {
+            _context.Books.Add(book);
+        }
+        else
+        {
+            bookInDb = book;
+        }
+
+        _context.SaveChanges();
+        return RedirectToAction("Index", "Home");
+    }
 
 }
 
